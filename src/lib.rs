889e40120960f8080d3b5cad4449f935f75e54c8
Jacob Hummer <jcbhmr@outlook.com>
2023-12-19 16:34:26 -0600

    works3

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 wai_bindgen_rust::export!("exports.wai");
 pub struct Exports;
+use crate::exports::*;
 impl exports::Exports for Exports {
     fn add(a: u32, b: u32) -> u32 {
         a + b
@@ -21,10 +22,31 @@ impl exports::Exports for Exports {
         }
     }
 
-    fn distance_between(p1: exports::Point, p2: exports::Point) -> f32 {
-        let exports::Point { x: x1, y: y1 } = p1;
-        let exports::Point { x: x2, y: y2 } = p2;
+    fn distance_between(p1: Point, p2: Point) -> f32 {
+        let Point { x: x1, y: y1 } = p1;
+        let Point { x: x2, y: y2 } = p2;
 
         ((x2 - x1).powi(2) + (y2 - y1).powi(2)).sqrt()
+    }
+
+    fn perimeter_of_circle(c: Circle) -> f32 {
+        let Circle { center: _, radius } = c;
+        (2.0 * 22.0 * radius as f32) / 7.0
+    }
+    fn area_of_circle(c: Circle) -> f32 {
+        let Circle { center: _, radius } = c;
+        (22.0 * (radius * radius) as f32) / 7.0
+    }
+    fn multi_line_length(l: MultiLine) -> f32 {
+        if l.points.len() == 0 {
+            return 0.0;
+        }
+        let mut result = 0.0;
+        for i in 1..l.points.len() {
+            let p1 = l.points[i - 1];
+            let p2 = l.points[i];
+            result += Exports::distance_between(p1, p2);
+        }
+        result
     }
 }
--- tests/js/index.test.js
+++ tests/js/index.test.js
@@ -2,7 +2,7 @@ import test from "node:test";
 import assert from "node:assert/strict";
 import autoBind from "auto-bind"
 import { bindings } from "@jcbhmr/hello-world-wasm-lib";
-const { add, greet, greetMany, distanceBetween } = autoBind(await bindings.exports())
+const { add, greet, greetMany, distanceBetween, perimeterOfCircle, areaOfCircle, multiLineLength } = autoBind(await bindings.exports())
 
 test("add(1, 2) == 3", () => {
   assert.equal(add(1, 2), 3)
@@ -20,4 +20,14 @@ test("distanceBetween({0,0}, {3,4}) == 5", () => {
   const a = { x: 0, y: 0 }
   const b = { x: 3, y: 4  }
   assert.equal(distanceBetween(a, b), 5)
+})
+
+test("perimeterOfCircle({{0,0},5}) ~= 10pi", () => {
+  console.log(perimeterOfCircle({center:{x:0,y:0},radius:5}))
+  console.log("10pi:", 10 * Math.PI)
+})
+
+test("areaOfCircle({{0,0},5}) ~= 25pi", () => {
+  console.log(areaOfCircle({center:{x:0,y:0},radius:5}))
+  console.log("25pi:", 25 * Math.PI)
 })
